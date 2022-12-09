b31474e581de58c4306ee76ec94c8934bfc5cacb
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-12-09 22:47:35 +0300

    delay

--- src/main/kotlin/KotlinCoroutines.kt
+++ src/main/kotlin/KotlinCoroutines.kt
@@ -1,1 +1,16 @@
-fun
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.*
+import kotlinx.coroutines.launch
+
+@OptIn(DelicateCoroutinesApi::class)
+fun main() {
+    println("Main program starts: ${Thread.currentThread().name}")
+
+    GlobalScope.launch {
+        println("Fake work starts: ${Thread.currentThread().name}")
+        delay(1000)
+        println("Fake work finish: ${Thread.currentThread().name}")
+    }
+    Thread.sleep(3000)
+    println("Main program ends: ${Thread.currentThread().name}")
+}
